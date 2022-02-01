5d475035342a51750ca6e32b50f3adeb98bf8dd9
redrocket83 <58429450+redrocket83@users.noreply.github.com>
2022-02-01 22:14:54 +0000

    Update Chapter2_Writing_Simple_Programs.py

--- Chapter2_Writing_Simple_Programs.py
+++ Chapter2_Writing_Simple_Programs.py
@@ -2,3 +2,55 @@
 # by John Zelle, 3rd Ed.
 # Chapter 2: Writing simple programs
 # End-of-Chapter Exercises
+
+# Review Questions
+
+# Q1. The best way to write a program is to immediately type in some code and then debug it until it works. FALSE
+#
+# 1A. Ref Page 27/28. "Writing large programs is daunting challenge. It would be almost impossible without a systematic approach. The process of creating a program is often
+#      broken down into stages according to the information that is produced in each phase."
+#
+# Q2. An algorithm can be written without using a programming language. TRUE
+#
+# A2. Ref Page 28. Analyse the problem, Determine specifications and what your program will do, Create a design. The main taks is to design the algorithm which will meet the 
+#     specification. Ref Page 30. An algorithm can be written using Pseudocode which is just precise English for describing what a program does.
+#
+# Q3. Programs no longer require modification after they are written and de-bugged. FALSE
+#
+# A3. Ref Page 28. One of the processes for creating a program involves testing and debugging the program. Programs are highly likely to be modified again and again after being
+#     created which in turn will need further testing and debugging.
+#
+# Q4. Python identifiers start with an underscore or a letter. TRUE
+#
+# A4. Ref Page 31. Python has rules about identifiers or names as they're also known by. Each identifier must begin with an underscore or a letter
+#
+# Q5. Keywords make good variable names. FALSE
+#
+# A5. Ref Page 31. Some identifiers are part of Python itself and should not be used as they have a completely different meaning or function.
+#
+# Q6. Expressions are built from literals, variables and operators. TRUE
+#
+# A6. Ref Page Page 32: "The simplest kind of expression is a literal." p.33 "A simple identifier can also be an expression. We use identifiers as variables to give names 
+#     to values. When an identifier appears as an expression, its value is retrieved to provide a result for the expression. p.34 "More complex and interesting expressions 
+#     can be constructed by combining simpler expressions with operators."
+#
+# Q7. In Python x = x + 1 is a legal expression. TRUE
+#
+# A7. Ref Page 37. A variable can be assigned many times and the current value of a variable can be used to update its value. See Figure 2.1 on Page 38.
+#
+# Q8. Python does not allow the input of multiple values within a single statement. FALSE
+#
+# A8. Ref Page 42. Simutaneous assignment can be used to get multiple values from a user in a single input. Example below
+#
+#     score1, score 2 = eval(input("Please enter 2 scores separated by a comma: "))
+#
+# Q9. A counted loop is designed to iterate a certain amount of times. TRUE
+#
+# A9. Ref Page 43/44. A definite loop will execute a definite number of times and is basically known as a counted loop
+#
+# for i in range(10): for example
+#
+# Q10. In a flowchart, diamonds are used to show statement sequences and rectangles are used for decision points. FALSE
+#
+# A10. Ref Page 46/47. Diamonds are used for decision points and rectangles are use to assign the next item in the sequence 
+
