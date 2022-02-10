52754c8426deca57a878ba3c33e19df9f8972d99
redrocket83 <58429450+redrocket83@users.noreply.github.com>
2022-02-10 23:19:52 +0000

    Update Chapter3_Computing_with_Numbers.py

--- Chapter3_Computing_with_Numbers.py
+++ Chapter3_Computing_with_Numbers.py
@@ -238,7 +238,17 @@ def main():
     print("The cost per square inch of the pizza is", round((cost/area), 2))
 main()
 #
-# Q3. 
+# Q3. Write a program that computes the molecular weight of a carbohydrate (in grams per mole) based on the number of hydrogen, carbon, and
+#     oxygen atoms in the molecule. The program should prompt the user to enter the number of hydrogen atomes, the number of carbon atoms,
+#     and the number of oxygen atoms. The program then prints the total combined molecular weight of all the atoms based on these individual
+#     atom weights:
+#     Atom      Weight
+#            (grams/mole)
+#     ______________________
+#      H         1.00894
+#      C         12.0107
+#      O         15.9994
+#     For example, the molecular weight of water (H20) is 2(1.00794) + 15.9994 = 18.01528.
 
 
 
