813555fbf447d5e73b62980539e1a32cf57f961d
redrocket83 <58429450+redrocket83@users.noreply.github.com>
2022-02-17 22:26:23 +0000

    Update Chapter4_Objects_and_Graphics.py

--- Chapter4_Objects_and_Graphics.py
+++ Chapter4_Objects_and_Graphics.py
@@ -154,9 +154,9 @@
 #
        from graphics import *
 
-       def main():                          # Defines the function named main() (i.e., the program)
-           win = GraphWin()                 # Openstucts GraphWin object with "Graphics Window" default title and default size 200 x 200 pixels
-           shape = Circle(Point(50,50), 20) # Constructs a circle object with center Point object at 50,50 and radius of 20 pixels
+       def main():                          # Defines the function named main() which contains the program
+           win = GraphWin()                 # Creates a GraphWin object with "Graphics Window" default title and default size 200 x 200 pixels
+           shape = Circle(Point(50,50), 20) # Constructs a circle object with the center at 50,50 and radius of 20 pixels
            shape.setOutline("red")          # Calls setOutline method of circle object named shape, seting its outline to the color red
            shape.setFill("red")             # Calls setFill method of circle object shape, it fills itself with the color red
            shape.draw(win)                  # Calls draw method of circle object named shape, it draws itself in graphics window object named win
@@ -169,8 +169,10 @@
                dy = p.getY() - c.get&()     # Assignment statement whereby variable dy is assigned a similar expression as dx, except this time it is for the Y axis
                shape.move(dx, dy)           # Calls the move() method of shape to move shape by the x distance referred to by dx and the y distance referred to by dy, thus
                                             # changing the state of shape by moving its center point to the new coordinates encapsulated in the event object p
-          win.close()                       # Calls close() method of graphics window object named, closing the graphics window after the loop is finished.
+          win.close()                       # Calls the close() method of the Graphics Window variable win, closing the graphics window after the loop is finished.
                                             # Note that it will not be possible to see the final location of the circle. To do so, comment out win.close() or call on win's
                                             # getMouse() method to make win close after a mouse click.
-       main()                               # Calls the function named main() (i.e., the program)
+       main()                               # Calls the function named main() to run the program
 
+# Programming Exercises
+#
