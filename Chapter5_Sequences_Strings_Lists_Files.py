4bed2d7e72b334ca5a45ea0baf9256dab6c33929
redrocket83 <58429450+redrocket83@users.noreply.github.com>
2022-02-19 16:10:46 +0000

    Create Chapter5_Sequences_Strings_Lists_Files.py

---
+++ Chapter5_Sequences_Strings_Lists_Files.py
@@ -0,0 +1,6 @@
+# "Python Programming: An Introduction to Computer Science, Third Edition" by John Zelle
+# Chapter 5: Sequences: Strings, Lists and Files
+# End-of-Chapter Exercises
+
+# REVIEW QUESTIONS
+#
