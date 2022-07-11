17f8c0be1e68fc1647ecffaa5b48cac8005ebab8
redrocket83 <58429450+redrocket83@users.noreply.github.com>
2022-07-11 20:33:42 +0100

    Update Chapter6_Defining_Functions.py

--- Chapter6_Defining_Functions.py
+++ Chapter6_Defining_Functions.py
@@ -463,3 +463,30 @@ def main():
     print(list)
 
 main()
+
+# Chapter 6 - Exercise 14
+# Sum of squares from file. I have a file called list.txt which contains numbers 1-10
+
+def toNumbers(strList):
+    for i in range(len(strList)):
+        strList[i] = float(strList[i])
+
+def sumList(nums):
+    total = 0
+    for n in nums:
+        total = total + n
+    return total
+
+def squareEach(nums):
+    for i in range(len(nums)):
+        nums[i] = nums[i]**2
+
+def main():
+    print("Program to find sum of squares from numbers in a file")
+    fname = input("Enter filename: ")
+    data = open(fname,'r').readlines()
+    toNumbers(data)
+    squareEach(data)
+    print("Sum of squares:", sumList(data))
+
+main()
